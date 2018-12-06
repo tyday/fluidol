0bcbbde7922a980859987d9908bb9b7ad3d0023b
Ty Day <tyrday@gmail.com>
2018-12-06 17:15:02 -0500

    add login

--- fluidol_index/urls.py
+++ fluidol_index/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index')
 ]
+#Add Django site authentication urls ( for login, logout, password management)
+urlpatterns += [ path('accounts/', include('django.contrib.auth.urls')),]
