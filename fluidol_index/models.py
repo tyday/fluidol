31584dc384d32402ed294ce4b836e15b3a862d8c
Ty Day <tyrday@gmail.com>
2018-12-12 14:39:05 -0500

    initial model creation

--- fluidol_index/models.py
+++ fluidol_index/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ManyToManyField(Category)
+    description = models.TextField()
+    main_photo = models.ImageField(null=True,blank=True)
+    
+    def __str__(self):
+        return self.name
