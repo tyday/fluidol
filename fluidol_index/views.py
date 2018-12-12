8a31388c62ef1703e9639c11e21bc1ee93e2f8b0
Ty Day <tyrday@gmail.com>
2018-12-12 16:01:03 -0500

    simple css and base_generic and index added

--- fluidol_index/views.py
+++ fluidol_index/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
+from .models import Product
 # Create your views here.
 
 @login_required
 def index(request):
-    return HttpResponse("helloooooo")
+    return render(request, 'index.html')
