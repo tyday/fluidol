da1dc84a0a98bf33458582cad2d6cd3d8775928b
Ty Day <tyrday@gmail.com>
2018-12-13 13:31:54 -0500

    remove extraneous text from views.py. Add extraneous spacing

--- fluidol_index/views.py
+++ fluidol_index/views.py
@@ -10,14 +10,10 @@ from .models import Product
 def index(request):
     return render(request, 'index.html')
 
+
 class ProductListView(generic.ListView):
     model = Product
+
+
 class ProductDetailView(generic.DetailView):
     model=Product
-# def product_detail_view(request, primary_key):
-#     try:
-#         product = Product.objects.get(pk=primary_key)
-#     except Product.DoesNotExist:
-#         raise Http404('Product does not exist')
-    
-#     return render(request, 'catalog/product_detail.html', context={'product':product})
